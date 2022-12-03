ada1b82e4ddc294c32d4f5a696d28d81ae31c89a
Muhammad Haggy <86955979+muhammadHaggy@users.noreply.github.com>
2022-12-03 13:11:28 +0700

    qr working

--- src/App.js
+++ src/App.js
@@ -3,44 +3,49 @@ import React, { Component } from "react";
 import QrReader from "modern-react-qr-reader";
 
 class Test extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      result: "No result"
+        this.state = {
+            result: "No result"
+        };
+
+        this.handleError = this.handleError.bind(this);
+        this.handleScan = this.handleScan.bind(this);
+    }
+
+    handleScan = (data) => {
+        if (data) {
+            this.setState({
+                result: data
+            });
+            console.log(this.state.result);
+            // this.setState({result: data});
+        }
     };
 
-    this.handleError = this.handleError.bind(this);
-    this.handleScan = this.handleScan.bind(this);
-  }
+    handleError = (err) => {
+        console.error(err);
+    };
 
-  handleScan = (data) => {
-    if (data) {
-      this.setState({
-        result: data
-      });
-      console.log(this.state.result);
-      // this.setState({result: data});
+    render() {
+        return (
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <QrReader
+                    delay={300}
+                    facingMode={"environment"}
+                    onError={this.handleError}
+                    onScan={this.handleScan}
+                    style={{ width: "80%" }}
+                />
+                
+                <p>{this.state.result}</p>
+            </div>
+        );
     }
-  };
-
-  handleError = (err) => {
-    console.error(err);
-  };
-
-  render() {
-    return (
-      <div>
-        <QrReader
-          delay={300}
-          facingMode={"environment"}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          style={{ width: "100%" }}
-        />
-        <p>{this.state.result}</p>
-      </div>
-    );
-  }
 }
 export default Test;
