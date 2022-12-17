27ca27fda2393f051cd1bf848b67fa4adde77963
Muhammad Haggy <86955979+muhammadHaggy@users.noreply.github.com>
2022-12-17 17:33:59 +0700

    limit to ean-13 barcode only

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 
 import React from 'react';
 import Html5QrcodePlugin from './Html5QrcodePlugin.jsx'
-import ResultContainerPlugin from './ResultContainerPlugin.jsx'
-import HowToUse from './HowToUse.jsx'
 import Header from './components/Header';
 import SwipeableEdgeDrawer from './ResultDrawer';
 
@@ -25,12 +23,12 @@ class App extends React.Component {
         <section className="App-section">
           <br />
           <br />
-          <br />
           <Html5QrcodePlugin
             fps={30}
             qrbox={250}
             disableFlip={false}
-            qrCodeSuccessCallback={this.onNewScanResult} />
+            qrCodeSuccessCallback={this.onNewScanResult}
+          />
           {/* <ResultContainerPlugin results={this.state.decodedResults} /> */}
         </section>
         <SwipeableEdgeDrawer results={this.state.decodedResults} />
@@ -45,7 +43,7 @@ class App extends React.Component {
     // let decodedResults = this.state.decodedResults;
     // decodedResults.push(decodedResult);
     this.setState((state, props) => {
-      if (JSON.stringify(state.decodedResults[state.decodedResults.length-1]) != JSON.stringify(decodedResult)) {
+      if (decodedResult.result.format.format === 9 && JSON.stringify(state.decodedResults[state.decodedResults.length - 1]) != JSON.stringify(decodedResult)) {
         state.decodedResults.push(decodedResult);
         return state;
       }
